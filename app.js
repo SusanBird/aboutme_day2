f19e9d98a73fd92a98acb1c799f8ed60b40cf695
SusanBird <susanbird391@gmail.com>
2022-03-01 15:04:39 -0800

    testing to see if updates change anything

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const animal = document.getElementById('animalDiv');
 /// console.log(showButton, animal, 'validate to make sure we grabbed them')
 // set event listeners
 showButton.addEventListener('click', () => {
-    // console.log(' show button clicked', 'validate button is connected')
-     /// this will remove the hidden class and display you favorite animal.       
+    // console.log('show button clicked', 'validate button is connected')
+     /// this will remove the hidden class and display your favorite animal.       
     animal.classList.remove('hidden');
 });
 
