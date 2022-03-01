f90a1d1a9acba7053d5ac9aa8c4e78c65edd0b4a
SusanBird <susanbird391@gmail.com>
2022-03-01 14:59:13 -0800

    pasted in html text from assignment

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // import functions
-
+// jk, no functions to import
 // grab DOM elements
+const showButton = document.getElementById('btn');
+const animal = document.getElementById('animalDiv');
+/// console.log(showButton, animal, 'validate to make sure we grabbed them')
+// set event listeners
+showButton.addEventListener('click', () => {
+    // console.log(' show button clicked', 'validate button is connected')
+     /// this will remove the hidden class and display you favorite animal.       
+    animal.classList.remove('hidden');
+});
 
-// set event listeners 
+
+
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
